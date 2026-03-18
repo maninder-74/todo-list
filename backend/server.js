042536d52b89b6e2b729c02d0e7533b3c39abaff
maninder-74 <maninder.dev74@gmail.com>
2026-03-18 13:43:19 +0530

    remove frontend serving

--- backend/server.js
+++ backend/server.js
@@ -17,12 +17,12 @@ app.use("/api/todos", todoRoutes);
 
 const __dirname = path.resolve();
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+  // app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  // app.get("*", (req, res) => {
+  //   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  // });
+// }
 
 app.listen(PORT, () => {
   connectDB();
